d507611d5bf79ca87d9255139bc5ae0e0a21afce
aninditaaghosh <aninditaaghosh@gmail.com>
2013-06-30 23:47:55 +0530

    problem in bundle install

---
+++ demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = '8ec01fe336721e11084620ce93906ab9e6f087917e8800c411b9d2cbebff89ead3da5d50986f6c54cacb8a38da81d2f0dabef248f356a2d747a46e7a34209d33'
